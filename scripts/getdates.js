c4926803bf4b2d6a7c48bbca7821698f87547305
cardealscript <cfelisberto@byupathway.edu>
2026-03-04 21:46:26 +0200

    home page

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Exibe o ano atual no footer
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Exibe a última modificação do documento
+document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
